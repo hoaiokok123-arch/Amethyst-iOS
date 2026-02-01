7f06efd9af1f29013744b19f6fe8ad7ec8009e84
hoaiokok123-arch <hoaiokok123@gmail.com>
2026-02-01 18:52:43 +0700

    Add files via upload
    Signed-off-by: hoaiokok123-arch <hoaiokok123@gmail.com>

---
+++ Natives/LauncherProfileEditorViewControllerBase.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+#import "PLPrefTableViewController.h"
+
+@interface LauncherProfileEditorViewControllerBase : PLPrefTableViewController
+@property(nonatomic) NSMutableDictionary* profile;
+@end
